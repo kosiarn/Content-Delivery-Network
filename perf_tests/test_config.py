c283b9f225626c6b613b244ffeb41ebc056e1dad
kosiarn <62908964+kosiarn@users.noreply.github.com>
2024-04-02 14:06:01 +0200

    add available attachments' names to test config

--- perf_tests/test_config.py
+++ perf_tests/test_config.py
@@ -1,2 +1,10 @@
 CENTRAL_SERVER_ADDRESS = "http://localhost:8002"
 EDGE_SERVER_ADDRESS = "http://localhost:8001"
+
+available_attachments = [
+    "broken_car.jpg",
+    "coffe_beans.jpg",
+    "fireworks.jpg",
+    "red_kite.jpg",
+    "squirrel.jpg"
+]

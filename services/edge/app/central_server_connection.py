2c5fa1f88eb35ecbe178fe3b8b26fa0719f905e4
kosiarn <62908964+kosiarn@users.noreply.github.com>
2024-04-02 13:29:51 +0200

    add debug info to central server log utility

--- services/edge/app/central_server_connection.py
+++ services/edge/app/central_server_connection.py
@@ -18,7 +18,7 @@ def fetchFromCentralServer(attachment_name):
     try:
         central_server_fetch_request = get(request_address)
     except:
-        return log("Request failed; central server rejected the request.")
+        return log(f"Request failed; central server rejected the request. Request URL: {request_address}")
     if central_server_fetch_request.status_code == 200:
         logCentralServerHit()
         return central_server_fetch_request.content
